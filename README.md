8fe2f0ba5aada2d4be2a1d4493278ba2be6764f4
clement <clement.bataille@epita.fr>
2025-11-15 19:19:28 +0100

    work to do

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 This project is has for objectif to connect in peer to peer pc so they can vote for one of the user to do something in the week or the day then you can use it to be display on some front page publish by the client or something like that. For now it is not very clear what i want to do with this, but i will sur have fun making it XD
 But i have a start of server to discover other server and a basic display on terminal 
+
+
+What i have now
+
+Client try to find a server
+then send a message to it so he can know who it is 
+
+
+TODO
+Save all the user
+Create a selecting list and a way to send a message to all the server that i know
+And refacto the code
--- main.py
+++ main.py
@@ -16,21 +16,25 @@ def get_next_port(indice : int) -> int:
 class server(th.Thread):
 
     def __init__(self,name : str,port : int,other_clients : list[tuple[str,int]],screen : display.screen):
-        th.Thread.__init__(self)
         self.name = name
+        self.port = port
+        self.other_clients = other_clients
+
+        #Thread setup
+        th.Thread.__init__(self)
         self.__stop_event = th.Event()
+
+        #Socket setup
         self.server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.server.bind(('127.0.0.1',port))
-        self.read_list = []
-        self.port = port
-        self.writable_list = []
-        self.other_clients = other_clients
+        self.read_list : list[socket.socket] = [self.server]
+        self.writable_list : list[socket.socket] = []
+
+        #Screen setup
         self.screen : display.screen = screen.split_vertical(0.5)
         self.std_screen = screen.get_screen(0)
         self.error_screen = screen.get_screen(1)
 
-
-        
 
     def stop(self):
         self.__stop_event.set()
@@ -78,8 +82,6 @@ class server(th.Thread):
         self.server.listen(1)
         self.std_screen.append(f"Server started on port : {self.port}")
         self.std_screen.append("Waiting for connection...")
-        self.read_list = [self.server]
-        self.writable_list : list[socket.socket] = []
         while not self.__stop_event.is_set():
             all_event = select.select(self.read_list, self.writable_list, [],3)
             readable : list[socket.socket] = all_event[0]
@@ -96,21 +98,34 @@ class client:
         self.name = name
         self.func = func
         self.other_clients = []
-        self.main_screen : display.screen = screen
-        self.main_screen.split_horizontally(0.5)
-        self.screen = self.main_screen.get_screen(1)
+
+        #Screen setup
+        self.main_screen : display.screen = screen #Main screen use to display everything
+        self.main_screen.split_horizontally(0.8) #Split between server screen and client screen
+        self.client_screen : display.screen = self.main_screen.get_screen(1) #Screen use to display client info
+        self.client_screen.split_vertical(0.8) #Split between client info and other clients
+        self.screen = self.client_screen.get_screen(0) #Screen use to display client info
+        self.other_screen = self.client_screen.get_screen(1) #Screen use to display other clients
+
+        #Server setup
         self.server = server("Server" + self.name,PORT,self.other_clients,self.main_screen.get_screen(0))
+
+        #For server finding
+        self.indice = 0
+        self.loocking_for_server = True
+
+        #Start of the main loop
         self.actif = True
         self.main()
-        
+
     def main(self):
         try:
             self.server.start()
             time.sleep(1)
-            self.find_server()
             while self.actif:
                 try:
                     self.main_screen.draw_terminal_screen()
+                    self.find_server()
                     time.sleep(0.1)
                 except KeyboardInterrupt:
                     self.stop()
@@ -127,26 +142,24 @@ class client:
         todo = True
 
     def find_server(self):
-        indice = 0
-        while (port := self.func(indice)) != -1:
-            self.main_screen.draw_terminal_screen()
-            indice += 1
-            if port == self.port:
-                continue
-            self.screen.append(f"Trying to connect to server on port {port}")
-            try:
-                self.client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-                self.client.connect((self.host,port))
-                self.client.send((f"Hello i am {self.name}").encode())
-                self.screen.append(f"Connected to server on port {port}")
-                self.screen.append(f"reviced: {self.client.recv(1024).decode()}")
-                self.client.close()
-            except ConnectionRefusedError:
-                pass
-            except OSError as e:
-                self.screen.append(f"OSError:{e}")
-            except KeyboardInterrupt:
-                self.stop()
+        if self.loocking_for_server:
+            if port := self.func(self.indice) != -1:
+                self.indice = 0
+            elif port == self.port:
+                self.indice += 1
+            else:
+                self.screen.append(f"Trying to connect to server on port {port}")
+                try:
+                    self.client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+                    self.client.connect((self.host,port))
+                    self.client.send((f"Hello i am {self.name}").encode())
+                    self.screen.append(f"Connected to server on port {port}")
+                    #self.screen.append(f"reviced: {self.client.recv(1024).decode()}") shoudl be in a non blocking read loop
+                    self.client.close()
+                except ConnectionRefusedError:
+                    pass
+                except OSError as e:
+                    self.screen.append(f"OSError:{e}")
 
 
 
